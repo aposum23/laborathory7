3cfe44e38702ae644ef01987387abb1c338d8df8
aposum23 <81620056+aposum23@users.noreply.github.com>
2021-11-19 17:08:35 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,11 +1,12 @@
-lst = input('Write list: ')
-lst = lst.split()
+if __name__ == '__main__':
+    lst = input('Write list: ')
+    lst = lst.split()
 
-num = 0
+    num = 0
 
-for ind, elem in enumerate(lst):
-    if elem == '0':
-        print(f"Zero's index: {ind}")
-        num += 1
+    for ind, elem in enumerate(lst):
+        if elem == '0':
+            print(f"Zero's index: {ind}")
+            num += 1
 
-print(f'Number of elements: {num}')
+    print(f'Number of elements: {num}')
